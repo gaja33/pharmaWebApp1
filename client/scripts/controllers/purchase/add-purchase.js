b8a3574b8d0548b226cba1faa83ea7b365b519a0
TPCSED\gspalankar <gajananpalankar33@gmail.com>
2017-12-04 17:06:51 +0530

    Purchase and medicine adding logic combined

--- client/scripts/controllers/purchase/add-purchase.js
+++ client/scripts/controllers/purchase/add-purchase.js
@@ -14,6 +14,7 @@ angular.module('kamakshiJewellersApp')
 		$scope.Add = true;
 
 		$scope.purchase = {};
+		$scope.medicine = {};
 	
 		$scope.dateOptions = {
             formatYear: 'yy',
@@ -25,19 +26,100 @@ angular.module('kamakshiJewellersApp')
         $scope.format = $scope.formats[0];
         $scope.altInputFormats = ['M!/d!/yyyy'];
 	
-		$scope.popup1 = {
+		$scope.InvDatePopup = {
             opened: false
         };
 
-        $scope.open1 = function () {
-            $scope.popup1.opened = true;
+        $scope.openInvDate = function () {
+            $scope.InvDatePopup.opened = true;
         };
-		
+    
+    
+        $scope.dateOptionsMan = {
+            formatYear: 'yyyy',
+            maxDate: new Date(2020, 5, 22),
+            startingDay: 1,
+            minMode: 'month'
+        };
+    
+        $scope.formatsMan = ['MMM-yyyy'];
+        $scope.formatman = $scope.formatsMan[0];
+    
+        $scope.ManDatePopup = {
+            opened: false
+        };
+
+        $scope.openManDate = function () {
+            $scope.ManDatePopup.opened = true;
+        };
+    
+        $scope.ExpDatePopup = {
+            opened: false
+        };
+
+        $scope.openExpDate = function () {
+            $scope.ExpDatePopup.opened = true;
+        };
+    
 	
 		$http.get('/api/suppliers').then(function (resp) {
-			console.log("resp", resp)
+			//console.log("resp", resp)
 			$scope.supplierNames = resp.data;
 		})
+        
+        
+        $http.get('/api/CategoryNames').then(function (resp) {
+            //console.log("resp", resp)
+            $scope.categoryNames = resp.data;
+        })
+        
+        $scope.getPrevBalance = function(supName){
+            console.log("supName",supName);
+            $http.get('/api/purchases?filter[where][supplier]='+supName).then(function(resp){
+                console.log("supDetails",resp.data[0].prevBalance);
+                $scope.preBalance = resp.data[0].prevBalance;
+            })
+        }
+        
+        $scope.medArray = [];
+        
+        $scope.addMedicines = function(medObj){
+            console.log("medObj",medObj);
+            medObj.total = medObj.quantity * medObj.buyPrice;
+            
+            $scope.medArray.push(medObj)
+            $scope.medicine = {};
+            $scope.Medicine1.$setPristine();
+            $scope.Medicine1.$setUntouched();
+            
+            $scope.purchase.subTotal = 0;
+            for(var i=0; i<$scope.medArray.length;i++){
+                $scope.purchase.subTotal += $scope.medArray[i].total;
+            }
+        }
+        
+        $scope.deleteMedData = function(idx){
+            $scope.medArray.splice(idx, 1);
+            $scope.purchase.subTotal = 0;
+            for(var i=0; i<$scope.medArray.length;i++){
+                $scope.purchase.subTotal += $scope.medArray[i].total;
+            }
+        }
+        
+        $scope.calculateTotalDiscount = function(dis,tot,prevB){
+            $scope.totalDis = (dis * tot) / 100;
+            
+            $scope.purchase.total = tot - $scope.totalDis;
+            
+            
+            $scope.purchase.grandTotal = $scope.purchase.total + prevB;
+        }
+        
+        
+        $scope.calculateBalance = function(p,gT){
+            $scope.purchase.prevBalance = gT-p;
+        }
+        
 
 		$scope.purchaseSave = function (obj) {
 			console.log("obj", obj)
