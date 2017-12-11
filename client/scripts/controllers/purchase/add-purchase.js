872ac9e1931bc0cdb65a98eb3dbf0c9f2b621376
gaja33 <gajananpalankar33@gmail.com>
2017-12-11 11:13:10 +0530

    some chnages made in purchase module adding per strip quantity for tablet category

--- client/scripts/controllers/purchase/add-purchase.js
+++ client/scripts/controllers/purchase/add-purchase.js
@@ -8,167 +8,177 @@
  * Controller of the jewelleryApp
  */
 angular.module('kamakshiJewellersApp')
-    .controller('AddPurchaseCtrl', function ($scope, $rootScope, $http, $location, $timeout, $routeParams,$route) {
+	.controller('AddPurchaseCtrl', function ($scope, $rootScope, $http, $location, $timeout, $routeParams, $route) {
 
-        $scope.Update = false;
-        $scope.Add = true;
+		$scope.Update = false;
+		$scope.Add = true;
 
-        $scope.purchase = {};
-        $scope.medicine = {};
+		$scope.purchase = {};
+		$scope.medicine = {};
 
-        $scope.dateOptions = {
-            formatYear: 'yy',
-            maxDate: new Date(2020, 5, 22),
-            startingDay: 1
-        };
+		$scope.dateOptions = {
+			formatYear: 'yy',
+			maxDate: new Date(2020, 5, 22),
+			startingDay: 1
+		};
 
-        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-        $scope.format = $scope.formats[0];
-        $scope.altInputFormats = ['M!/d!/yyyy'];
+		$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+		$scope.format = $scope.formats[0];
+		$scope.altInputFormats = ['M!/d!/yyyy'];
 
-        $scope.InvDatePopup = {
-            opened: false
-        };
+		$scope.InvDatePopup = {
+			opened: false
+		};
 
-        $scope.openInvDate = function () {
-            $scope.InvDatePopup.opened = true;
-        };
-
-
-        $scope.dateOptionsMan = {
-            formatYear: 'yyyy',
-            maxDate: new Date(2020, 5, 22),
-            startingDay: 1,
-            minMode: 'month'
-        };
-
-        $scope.formatsMan = ['MMM-yyyy'];
-        $scope.formatman = $scope.formatsMan[0];
-
-        $scope.ManDatePopup = {
-            opened: false
-        };
-
-        $scope.openManDate = function () {
-            $scope.ManDatePopup.opened = true;
-        };
-
-        $scope.ExpDatePopup = {
-            opened: false
-        };
-
-        $scope.openExpDate = function () {
-            $scope.ExpDatePopup.opened = true;
-        };
+		$scope.openInvDate = function () {
+			$scope.InvDatePopup.opened = true;
+		};
 
 
-        $http.get('/api/suppliers').then(function (resp) {
-            //console.log("resp", resp)
-            $scope.supplierNames = resp.data;
-        })
+		$scope.dateOptionsMan = {
+			formatYear: 'yyyy',
+			maxDate: new Date(2020, 5, 22),
+			startingDay: 1,
+			minMode: 'month'
+		};
+
+		$scope.formatsMan = ['MMM-yyyy'];
+		$scope.formatman = $scope.formatsMan[0];
+
+		$scope.ManDatePopup = {
+			opened: false
+		};
+
+		$scope.openManDate = function () {
+			$scope.ManDatePopup.opened = true;
+		};
+
+		$scope.ExpDatePopup = {
+			opened: false
+		};
+
+		$scope.openExpDate = function () {
+			$scope.ExpDatePopup.opened = true;
+		};
 
 
-        $http.get('/api/CategoryNames').then(function (resp) {
-            //console.log("resp", resp)
-            $scope.categoryNames = resp.data;
-        })
-
-        $scope.getPrevBalance = function (supName) {
-            console.log("supName", supName);
-            $http.get('/api/purchases?filter[where][supplier]=' + supName).then(function (resp) {
-                console.log("supDetails", resp.data);
-                if (resp.data.length == 0) {
-                    $scope.preBalance = 0;
-                } else {
-                    $scope.preBalance = resp.data[resp.data.length - 1].prevBalance;
-                }
-            })
-        }
-
-        $scope.medArray = [];
-
-        $scope.addMedicines = function (medObj) {
-            console.log("medObj", medObj);
-            medObj.total = medObj.quantity * medObj.buyPrice;
-
-            $scope.medArray.push(medObj)
-            $scope.medicine = {};
-            $scope.Medicine1.$setPristine();
-            $scope.Medicine1.$setUntouched();
-
-            $scope.purchase.subTotal = 0;
-            for (var i = 0; i < $scope.medArray.length; i++) {
-                $scope.purchase.subTotal += $scope.medArray[i].total;
-            }
-        }
-
-        $scope.deleteMedData = function (idx) {
-            $scope.medArray.splice(idx, 1);
-            $scope.purchase.subTotal = 0;
-            for (var i = 0; i < $scope.medArray.length; i++) {
-                $scope.purchase.subTotal += $scope.medArray[i].total;
-            }
-        }
-
-        $scope.calculateTotalDiscount = function (dis, tot, prevB) {
-            $scope.totalDis = (dis * tot) / 100;
-
-            $scope.purchase.total = tot - $scope.totalDis;
+		$http.get('/api/suppliers').then(function (resp) {
+			//console.log("resp", resp)
+			$scope.supplierNames = resp.data;
+		})
 
 
-            $scope.purchase.grandTotal = $scope.purchase.total + prevB;
-        }
+		$http.get('/api/CategoryNames').then(function (resp) {
+			//console.log("resp", resp)
+			$scope.categoryNames = resp.data;
+		})
+
+		$scope.preBalance = 0;
+
+		$scope.getPrevBalance = function (supName) {
+			console.log("supName", supName);
+			$http.get('/api/purchases?filter[where][supplier]=' + supName).then(function (resp) {
+				console.log("supDetails", resp.data);
+				if (resp.data.length == 0) {
+					$scope.preBalance = 0;
+				} else {
+					$scope.preBalance = resp.data[resp.data.length - 1].prevBalance;
+				}
+			})
+		}
+
+		$scope.medArray = [];
+
+		$scope.addMedicines = function (medObj) {
+			console.log("medObj", medObj);
+			medObj.total = medObj.quantity * medObj.buyPrice;
+
+			$scope.medArray.push(medObj)
+			$scope.medicine = {};
+			$scope.Medicine1.$setPristine();
+			$scope.Medicine1.$setUntouched();
+
+			$scope.purchase.subTotal = 0;
+			for (var i = 0; i < $scope.medArray.length; i++) {
+				$scope.purchase.subTotal += $scope.medArray[i].total;
+			}
+		}
+
+		$scope.deleteMedData = function (idx) {
+			$scope.medArray.splice(idx, 1);
+			$scope.purchase.subTotal = 0;
+			for (var i = 0; i < $scope.medArray.length; i++) {
+				$scope.purchase.subTotal += $scope.medArray[i].total;
+			}
+		}
+
+		$scope.calculateTotalDiscount = function (dis, tot, prevB) {
+			$scope.totalDis = (dis * tot) / 100;
+
+			$scope.purchase.total = tot - $scope.totalDis;
 
 
-        $scope.calculateBalance = function (p, gT) {
-            $scope.purchase.prevBalance = gT - p;
-        }
+			$scope.purchase.grandTotal = $scope.purchase.total + prevB;
+		}
 
 
-        $scope.purchaseSave = function (pObj, mObj) {
-            console.log("pObj", pObj)
-            console.log("mObj", mObj)
-
-            var purchaseID;
-
-            $http.post('/api/purchases', pObj).then(function (resp) {
-                    console.log("resp", resp)
-                    purchaseID = resp.data.id;
-
-                    /*if (resp.status == 200) {
-                    	$scope.purchase = {};
-                    	$scope.Purchase.$setPristine();
-                    	$scope.Purchase.$setUntouched();
-
-                    	$timeout(function () {
-                    		$location.path('/view-purchase')
-                    	}, 500)
-
-                    }*/
-                }).catch(function (response) {
-                    console.error('Error', response.status, response.data);
-                })
-                .finally(function () {
-                    for (var i = 0; i < mObj.length; i++) {
-                        mObj[i].purchaseId = purchaseID;
-                    }
-                    console.log("DONE");
-                    console.log(mObj);
-                });
+		$scope.calculateBalance = function (p, gT) {
+			$scope.purchase.prevBalance = gT - p;
+		}
 
 
+		$scope.purchaseSave = function (pObj, mObj) {
+			console.log("pObj", pObj)
+			console.log("mObj", mObj)
+
+			var purchaseID;
+
+			$http.post('/api/purchases', pObj).then(function (resp) {
+					console.log("resp", resp)
+					purchaseID = resp.data.id;
+
+					/*if (resp.status == 200) {
+						$scope.purchase = {};
+						$scope.Purchase.$setPristine();
+						$scope.Purchase.$setUntouched();
+
+						$timeout(function () {
+							$location.path('/view-purchase')
+						}, 500)
+
+					}*/
+				}).catch(function (response) {
+					console.error('Error', response.status, response.data);
+				})
+				.finally(function () {
+					for (var i = 0; i < mObj.length; i++) {
+						mObj[i].purchaseId = purchaseID;
+
+						if (mObj[i].category == 'Tablet' || mObj[i].category == 'tablet') {
+							mObj[i].quantity = mObj[i].quantity * mObj[i].quantityInStrips;
+							mObj[i].pricePerUnit = (mObj[i].mrp/mObj[i].quantity).toFixed(2);
+							
+						}else{
+							mObj[i].quantity = mObj[i].quantity;
+							mObj[i].pricePerUnit = mObj[i].mrp;
+						}
+					}
+					console.log("DONE");
+				});
+			
+			
+					console.log("After Post",mObj);
 
 
 
+			$timeout(function () {
+				$http.post('/api/medicines', mObj).then(function (resp) {
+					console.log("Mresp", resp)
+				})
+			}, 1000)
 
-            $timeout(function () {
-                $http.post('/api/medicines', mObj).then(function (resp) {
-                    console.log("Mresp", resp)
-                })
-            }, 500)
-            
-            $route.reload();
-        }
+			$route.reload();
+		}
 
 
-    });
+	});
--- server/boot/auto-update.js
+++ server/boot/auto-update.js
@@ -1,11 +1,26 @@
 'use strict';
 
-module.exports = function(app) {
-  app.dataSources.postgres.autoupdate(function(err, result) {
-    if (err) {
-      console.log('err:' + err);
-    } else {
-      console.log('result1' + result);
-    }
-  });
+/*module.exports = function (app) {
+	app.dataSources.postgres.autoupdate(function (err, result) {
+		if (err) {
+			console.log('err:' + err);
+		} else {
+			console.log('result1' + result);
+		}
+	});
+};*/
+
+
+module.exports = function (app) {
+	var db = app.datasources.postgres;
+
+	db.autoupdate(function (err, result) {
+		//if (err) throw err;
+		if (err) {
+			console.log('err:' + err);
+		} else {
+			console.log('result' + result);
+		}
+		console.log('\nAutomigrate completed');
+	});
 };
